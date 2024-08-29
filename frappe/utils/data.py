11ba89b71d236dc7fa641924ef253c318bd1e5cf
Eureka ERP <admin@eureka-erp.com>
2024-08-29 15:45:08 +0000

    Add custom money format function for comission report

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -1267,6 +1267,90 @@ def fmt_money(
 
 	return amount
 
+def fmt_money_custom(
+	amount: str | float | int | None,
+	precision: int | None = None,
+	currency: str | None = None
+) -> str:
+	"""
+	Convert to string with commas for thousands, millions etc
+	"""
+	number_format = "#.###,##"
+	if precision is None:
+		precision = cint(frappe.db.get_default("currency_precision")) or None
+
+	decimal_str, comma_str, number_format_precision = ',', '.', 2
+
+	if precision is None:
+		precision = number_format_precision
+
+	# 40,000 -> 40,000.00
+	# 40,000.00000 -> 40,000.00
+	# 40,000.23000 -> 40,000.23
+
+	if isinstance(amount, str):
+		amount = flt(amount, precision)
+
+	if amount is None:
+		amount = 0
+
+	if decimal_str:
+		decimals_after = str(round(amount % 1, precision))
+		parts = decimals_after.split(".")
+		parts = parts[1] if len(parts) > 1 else parts[0]
+		decimals = parts
+		if precision > 2:
+			if len(decimals) < 3:
+				if currency:
+					fraction = frappe.db.get_value("Currency", currency, "fraction_units", cache=True) or 100
+					precision = len(cstr(fraction)) - 1
+				else:
+					precision = number_format_precision
+			elif len(decimals) < precision:
+				precision = len(decimals)
+
+	amount = "%.*f" % (precision, round(flt(amount), precision))
+
+	if amount.find(".") == -1:
+		decimals = ""
+	else:
+		decimals = amount.split(".")[1]
+
+	parts = []
+	minus = ""
+	if flt(amount) < 0:
+		minus = "-"
+
+	amount = cstr(abs(flt(amount))).split(".", 1)[0]
+
+	if len(amount) > 3:
+		parts.append(amount[-3:])
+		amount = amount[:-3]
+
+		val = number_format == "#,##,###.##" and 2 or 3
+
+		while len(amount) > val:
+			parts.append(amount[-val:])
+			amount = amount[:-val]
+
+	parts.append(amount)
+
+	parts.reverse()
+
+	amount = comma_str.join(parts) + ((precision and decimal_str) and (decimal_str + decimals) or "")
+	if amount != "0":
+		amount = minus + amount
+
+	if currency and frappe.defaults.get_global_default("hide_currency_symbol") != "Yes":
+		symbol = frappe.db.get_value("Currency", currency, "symbol", cache=True) or currency
+		symbol_on_right = frappe.db.get_value("Currency", currency, "symbol_on_right", cache=True)
+
+		if symbol_on_right:
+			amount = f"{amount}{frappe._(symbol)}"
+		else:
+			amount = f"{frappe._(symbol)}{amount}"
+
+	return amoun
 
 number_format_info = {
 	"#,###.##": (".", ",", 2),
